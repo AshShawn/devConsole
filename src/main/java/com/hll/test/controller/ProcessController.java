885c2ccda7fb41b99749ff558090b6b8d40676dc
sq <598581474@qq.com>
2019-06-04 16:15:09 +0800

    *增加上传下载功能

--- src/main/java/com/hll/test/controller/ProcessController.java
+++ src/main/java/com/hll/test/controller/ProcessController.java
@@ -2,20 +2,32 @@ package com.hll.test.controller;/**
  * Create by sq598 on 2019/3/12
  */
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.net.URLEncoder;
+import java.util.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import com.firenio.baseio.common.Cryptos;
 import com.hll.test.common.*;
@@ -54,10 +66,10 @@ public class ProcessController {
 
     @RequestMapping("/updateProcessInfo")
     @ResponseBody
-    public Response updateOperatorInfo(ProcessInfo info,HttpServletRequest request) {
+    public Response updateOperatorInfo(ProcessInfo info, HttpServletRequest request) {
         UserInfo userinfo = SessionUtil.getUserinfo(request);
         Integer isLeader = userinfo.getIsLeader();
-        if (isLeader==0){
+        if (isLeader == 0) {
             return Response.NO_PERMISSION;
         }
         if (info == null) {
@@ -69,9 +81,9 @@ public class ProcessController {
 
     @RequestMapping("/deleteProcessInfo")
     @ResponseBody
-    public Response updateOperatorInfo(Integer processID,HttpServletRequest request) {
+    public Response updateOperatorInfo(Integer processID, HttpServletRequest request) {
         UserInfo userinfo = SessionUtil.getUserinfo(request);
-        if (userinfo.getRoleID()!=3){
+        if (userinfo.getRoleID() != 3) {
             return Response.NO_PERMISSION;
         }
         if (processID == null) {
@@ -95,10 +107,10 @@ public class ProcessController {
         if (info == null) {
             return Response.LACK_OF_PARAM;
         }
-        if (info.getCurrentTaskType()!=0){
+        if (info.getCurrentTaskType() != 0) {
             return Response.err("当前只能创建需求任务");
         }
-        if (info.getDevUserID()==null){
+        if (info.getDevUserID() == null) {
             return Response.err("必须指定开发人员");
         }
         info.setCreateUserID(currUser.getUserID());
@@ -106,6 +118,104 @@ public class ProcessController {
         info.setIsComplete(0);
         processInfoMapper.insertSelective(info);
         return Response.SUCCESS;
+    }
+
+    @RequestMapping("/upload")
+    @ResponseBody
+    public Response upload(Integer processID, HttpServletRequest request) throws IOException {
+        //将当前上下文初始化给  CommonsMutipartResolver （多部分解析器）
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(
+                request.getSession().getServletContext());
+        multipartResolver.setDefaultEncoding("UTF-8");
+        multipartResolver.setMaxUploadSize(3 * 1024 * 1024);  //3mb
+        //检查form中是否有enctype="multipart/form-data"
+        if (multipartResolver.isMultipart(request)) {
+            MultipartFile file = null;
+            //将request变成多部分request
+            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+            multiRequest.setCharacterEncoding("UTF-8");
+            //获取multiRequest 中所有的文件名
+            Iterator iter = multiRequest.getFileNames();
+            while (iter.hasNext()) {
+                //一次遍历所有文件
+                file = multiRequest.getFile(iter.next().toString());
+                if (file != null) {
+                    String path = System.getProperty("user.dir") + "\\src\\main\\webapp\\upload\\" + file.getOriginalFilename();
+                    //上传
+                    file.transferTo(new File(path));
+                }
+            }
+            ProcessInfo processInfo = processInfoMapper.selectByPrimaryKey(processID);
+            String fileUrl = processInfo.getFileUrl();
+            if (StringUtil.isNullOrBlank(fileUrl)) {
+                processInfo.setFileUrl(file.getOriginalFilename() + ";");
+            } else {
+                processInfo.setFileUrl(fileUrl + file.getOriginalFilename()+";");
+            }
+            processInfoMapper.updateByPrimaryKeySelective(processInfo);
+        } else {
+            return Response.err("上传失败,请查看enctype类型");
+        }
+        return Response.SUCCESS;
+    }
+
+
+    @RequestMapping("/download")
+    @ResponseBody
+    public ResponseEntity<byte[]> download(String filename, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String downloadFilePath = System.getProperty("user.dir") + "\\src\\main\\webapp\\upload\\";//从我们的上传文件夹中去取
+
+        File file = new File(downloadFilePath + filename);//新建一个文件
+
+        HttpHeaders headers = new HttpHeaders();//http头信息
+
+        String downloadFileName = new String(filename.getBytes("UTF-8"), "iso-8859-1");//设置编码
+
+        headers.setContentDispositionFormData("attachment", downloadFileName);
+
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+
+        //MediaType:互联网媒介类型  contentType：具体请求中的媒体类型信息
+
+        return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file), headers, HttpStatus.CREATED);
+    }
+
+    @RequestMapping("/download2")
+    @ResponseBody
+    public ResponseEntity<byte[]> download2(String filenames, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        //需要压缩的文件
+        List<String> list = new ArrayList<String>();
+        String[] split = filenames.split(";");
+        for (String s : split) {
+            if (StringUtil.isNullOrBlank(s)){
+                continue;
+            }
+            list.add(s);
+        }
+
+        //压缩后的文件
+        String resourcesName = System.currentTimeMillis()+".zip";
+
+        ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(System.getProperty("user.dir") + "\\src\\main\\webapp\\download\\"+resourcesName));
+        InputStream input = null;
+
+        for (String str : list) {
+            String name = System.getProperty("user.dir") + "\\src\\main\\webapp\\upload\\"+str;
+            input = new FileInputStream(new File(name));
+            zipOut.putNextEntry(new ZipEntry(str));
+            int temp = 0;
+            while((temp = input.read()) != -1){
+                zipOut.write(temp);
+            }
+            input.close();
+        }
+        zipOut.close();
+        File file = new File(System.getProperty("user.dir") + "\\src\\main\\webapp\\download\\"+resourcesName);
+        HttpHeaders headers = new HttpHeaders();
+        String filename = new String(resourcesName.getBytes("utf-8"),"iso-8859-1");
+        headers.setContentDispositionFormData("attachment", filename);
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),headers,HttpStatus.CREATED);
     }
 
 
